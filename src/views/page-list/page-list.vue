86b54d26d8a5f6a4a643c63af099d061ae5654bc
zchong <zhch_hong@163.com>
2021-03-24 20:35:36 +0800

    feat: 元素的大小调整和拖动

--- src/views/page-list/page-list.vue
+++ src/views/page-list/page-list.vue
@@ -1,36 +1,107 @@
 <template>
-  <div class="page-list" @drop="ondrop" @dragover="ondragover">
-    <span style="position: relative" draggable="true" @dragstart="ondragstart" @dragend="ondragend">page-list</span>
+  <div class="page-list" @drop="ondrop" @dragover="ondragover" @dragenter="ondragenter" @dragleave="ondragleave">
+    <div ref="DRAGGABLE" class="drag" draggable="true" @drag="ondrag" @dragstart="ondragstart" @dragend="ondragend">
+      page-list
+    </div>
   </div>
 </template>
 <script>
+/* eslint-disable no-unused-vars */
+
+let offsetX = 0,
+  offsetY = 0;
+
 export default {
+  mounted() {
+    const el = this.$refs.DRAGGABLE;
+    el.onresize = (e) => {
+      console.log(10);
+    };
+  },
+
   methods: {
+    // ------------------------ 拖拽元素
+
+    /**
+     * 开始
+     */
     ondragstart(e) {
-      console.log(e);
+      offsetX = e.offsetX;
+      offsetY = e.offsetY;
+      // console.log(top, left, e.clientX, e.clientY);
     },
 
+    /**
+     * 移动
+     */
+    ondrag(e) {
+      // console.log('drag');
+    },
+
+    /**
+     * 结束
+     */
     ondragend(e) {
-      console.log('offset', e.offsetX, e.offsetY);
-      console.log('client', e.clientX, e.clientY);
-      e.target.style.top = e.offsetY + 'px';
-      e.target.style.left = e.offsetX + 'px';
+      e.target.style.top = e.clientY - offsetY + 'px';
+      e.target.style.left = e.clientX - 180 - offsetX + 'px';
+      // console.log('dragend');
+      // console.log(e.target.style, e.target.style);
+      // console.log('offset', e.offsetX, e.offsetY);
+      // console.log('client', e.clientX, e.clientY);
+      // e.target.style.top = e.offsetY + 'px';
+      // e.target.style.left = e.offsetX + 'px';
     },
 
+    // ------------------------ 放置区域
+
+    /**
+     * 进入
+     */
+    ondragenter(e) {
+      e.preventDefault();
+      // console.log('dragenter');
+    },
+
+    /**
+     * 离开
+     */
+    ondragleave(e) {
+      e.preventDefault();
+      // console.log('dragleave');
+    },
+
+    /**
+     * 移动
+     */
     ondragover(e) {
       e.preventDefault();
-      // console.log(e);
+      // console.log('dragover');
     },
 
+    /**
+     * 完成
+     */
     ondrop(e) {
       e.preventDefault();
-      console.log(e);
+      // console.log('drop');
     },
   },
 };
 </script>
 <style lang="scss" scoped>
 div.page-list {
+  position: relative;
   height: 100%;
+
+  div.drag {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100px;
+    height: 100px;
+    resize: both;
+    overflow: auto;
+    background-color: aquamarine;
+  }
 }
 </style>
